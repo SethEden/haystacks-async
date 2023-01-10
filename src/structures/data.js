f57fadb872f59f67c78aa60d1337e81063d37853
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-01-09 20:03:09 -0600

    hotFixPluginHaystacksDataDependency: Try to use a dependency injection pattern to effectively clone the Haystacks instance to the plugin instance of Haystacks.

--- src/structures/data.js
+++ src/structures/data.js
@@ -17,6 +17,34 @@ import hayConst from '@haystacks/constants';
 const {wrd} = hayConst;
 let data = {};
 
+/**
+ * @function getData
+ * @description returns the contents of the data structure. Can be used to stash data,
+ * before it is reassigned, for the purpose of setting up a unit test scenario with mocked data.
+ * @return {object} A JSON object that contains all of the data that is currently stored in the data structure.
+ * @author Seth Hollingsead
+ * @date 2023/01/09
+ */
+async function getData() {
+  return data;
+}
+
+/**
+ * @function setData
+ * @description Clears the data and sets it to some new specified data.
+ * Can be used to setup mock data, or create a clone of Haystacks for the purposes of
+ * executing a callback on Haystacks such as loading more data for a plugin.
+ * @param {object} newData The new data that should replace any data already stored on the data structure.
+ * @author Seth Hollingsead
+ * @date 2023/01/09
+ */
+async function setData(newData) {
+  data = {};
+  data = newData;
+}
+
 export default {
-  [wrd.cdata]: data
+  [wrd.cdata]: data,
+  getData,
+  setData
 };
