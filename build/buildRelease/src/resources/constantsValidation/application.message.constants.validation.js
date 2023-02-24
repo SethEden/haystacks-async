973687ebc4c74d1d37adf23e10c142e952721907
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-02-24 17:08:55 -0600

    cleanup: Clean up some code in the testHarness.

--- build/buildRelease/src/resources/constantsValidation/application.message.constants.validation.js
+++ build/buildRelease/src/resources/constantsValidation/application.message.constants.validation.js
@@ -41,7 +41,6 @@ export const applicationMessageConstantsValidation = [
   {Name: 'cresolvedConstantsPath_ApplicationCommandIs', Actual: app_msg.cresolvedConstantsPath_ApplicationCommandIs, Expected: 'resolvedConstantsPath_ApplicationCommand is: '},
   {Name: 'cresolvedConstantsPath_ApplicationConfigurationIs', Actual: app_msg.cresolvedConstantsPath_ApplicationConfigurationIs, Expected: 'resolvedConstantsPath_ApplicationConfiguration is: '},
   {Name: 'cresolvedConstantsPath_ApplicationConstantsIs', Actual: app_msg.cresolvedConstantsPath_ApplicationConstantsIs, Expected: 'resolvedConstantsPath_ApplicationConstants is: '},
-  {Name: 'cresolvedConstantsPath_ApplicationFunctionIs', Actual: app_msg.cresolvedConstantsPath_ApplicationFunctionIs, Expected: 'resolvedConstantsPath_ApplicationFunction is: '},
   {Name: 'cresolvedConstantsPath_ApplicationMessageIs', Actual: app_msg.cresolvedConstantsPath_ApplicationMessageIs, Expected: 'resolvedConstantsPath_ApplicationMessage is: '},
   {Name: 'cresolvedConstantsPath_ApplicationSystemIs', Actual: app_msg.cresolvedConstantsPath_ApplicationSystemIs, Expected: 'resolvedConstantsPath_ApplicationSystem is: '},
 
@@ -49,7 +48,6 @@ export const applicationMessageConstantsValidation = [
   {Name: 'cApplicationCommandConstantsPhase1Validation', Actual: app_msg.cApplicationCommandConstantsPhase1Validation, Expected: 'Application Command Constants Phase 1 Validation'},
   {Name: 'cApplicationConfigurationConstantsPhase1Validation', Actual: app_msg.cApplicationConfigurationConstantsPhase1Validation, Expected: 'Application Configuration Constants Phase 1 Validation'},
   {Name: 'cApplicationConstantsPhase1Validation', Actual: app_msg.cApplicationConstantsPhase1Validation, Expected: 'Application Constants Phase 1 Validation'},
-  {Name: 'cApplicationFunctionConstantsPhase1Validation', Actual: app_msg.cApplicationFunctionConstantsPhase1Validation, Expected: 'Application Function Constants Phase 1 Validation'},
   {Name: 'cApplicationMessageConstantsPhase1Validation', Actual: app_msg.cApplicationMessageConstantsPhase1Validation, Expected: 'Application Message Constants Phase 1 Validation'},
   {Name: 'cApplicationSystemConstantsPhase1Validation', Actual: app_msg.cApplicationSystemConstantsPhase1Validation, Expected: 'Application System Constants Phase 1 Validation'},
 
@@ -57,7 +55,6 @@ export const applicationMessageConstantsValidation = [
   {Name: 'cApplicationCommandConstantsPhase2Validation', Actual: app_msg.cApplicationCommandConstantsPhase2Validation, Expected: 'Application Command Constants Phase 2 Validation'},
   {Name: 'cApplicationConfigurationConstantsPhase2Validation', Actual: app_msg.cApplicationConfigurationConstantsPhase2Validation, Expected: 'Application Configuration Constants Phase 2 Validation'},
   {Name: 'cApplicationConstantsPhase2Validation', Actual: app_msg.cApplicationConstantsPhase2Validation, Expected: 'Application Constants Phase 2 Validation'},
-  {Name: 'cApplicationFunctionConstantsPhase2Validation', Actual: app_msg.cApplicationFunctionConstantsPhase2Validation, Expected: 'Application Function Constants Phase 2 Validation'},
   {Name: 'cApplicationMessageConstantsPhase2Validation', Actual: app_msg.cApplicationMessageConstantsPhase2Validation, Expected: 'Application Message Constants Phase 2 Validation'},
   {Name: 'cApplicationSystemConstantsPhase2Validation', Actual: app_msg.cApplicationSystemConstantsPhase2Validation, Expected: 'Application System Constants Phase 2 Validation'},
 ];
