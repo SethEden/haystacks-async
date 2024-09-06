a402e4f955349b4048812b22957e69a9ab021f9d
SethEden <Iceversaka@hotmail.com>
2024-09-06 15:02:18 -0500

    multiCurriculum: Committing all of the changes needed to support the latest release of Haystacks-TT multi-curriculum feature.

--- src/businessRules/rules/stringParsingUtilities.js
+++ src/businessRules/rules/stringParsingUtilities.js
@@ -139,7 +139,7 @@ async function stringToDataType(inputData, inputMetaData) {
   // console.log(`inputData is: ${JSON.stringify(inputData)}`);
   // console.log(`inputMetaData is: ${JSON.stringify(inputMetaData)}`);
   let returnData = false;
-  if (inputData) {
+  if (inputData !== undefined) {
     let dataType = await determineObjectDataType(inputData, '');
     switch (dataType) {
       case wrd.cBoolean:
